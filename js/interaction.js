c9e30e7676f374fdf45efacbd9312c220f10583f
sarahkim711 <ykim134@artic.edu>
2021-02-19 14:14:06 -0600

    first atte

--- js/interaction.js
+++ js/interaction.js
@@ -1,38 +1,106 @@
 
-let ellipseX =900;
-let ellipseY = 500;
+let button;
 
-function setup(){
+let fb;
+let google;
+let ig;
+let backgroundimg;
+
+let backgroundimgX =0
+let backgroundimgY =0
+let fbposX=0
+let fbposY=0
+let fbposYspeed = 3;
+let googleposX =0
+let googleposY = 0
+let googleposYspeed = 3;
+let googleposXspeed = 3;
+let igposX = 0
+let igpoxY = 0
+let igposYspeed = 3;
+
+
+
+function preload(){ ///////////////////////////////////////////////////
+
+  fb = loadImage("images/fb1swirl.png");
+  google = loadImage("images/g2swirl.png");
+  ig = loadImage("images/ig3swirl.png");
+  backgroundimg = loadImage ("images/backgroundswirl.png");
+}
+
+function setup(){ ///////////////////////////////////////////////////
   createCanvas(windowWidth,windowHeight);
+  imageMode(CENTER);
+  //define image x,y positions
+  backgroundimgX = windowWidth/2
+  backgroundimgY = windowHeight/2
+  fbposX=windowWidth/6
+  fbposY=windowHeight/2
+  googleposX = random(windowWidth);
+  googleposY = random(windowHeight);
+  // googleposX = windowWidth/2
+  // googleposY = windowHeight/2
+  igposX = windowWidth/1.2
+  igposY= windowHeight/2
 
 
 
 }
 
-function draw(){
-  if(mouseIsPressed){
-    background(10,30,69);
+
+function draw(){ ///////////////////////////////////////////////////
+  image (backgroundimg,backgroundimgX, backgroundimgY, windowWidth,windowHeight);
+  //IMAGES DRAWN 1,2,3
+  //  image(fb, windowWidth/6, windowHeight/2,250,250);
+  image(fb, fbposX, fbposY,250,250);
+  //image(google, windowWidth/2,windowHeight/2, 250,250);
+  image(google, googleposX,googleposY, 250,250);
+  //image(ig, windowWidth/1.2,windowHeight/2, 250,250);
+  image(ig, igposX,igposY, 250,250)
+
+
+  //animating IMAGES
+  //FACEBOOOOk
+    if (fbposY > windowHeight || fbposY < 0){
+      fbposYspeed= fbposYspeed * -1;
+    }
+  fbposY = fbposY + fbposYspeed;
+  fbposX = fbposX + random(-10,10);
+
+
+
+  //GOOOGLEEEEE
+  if (googleposY > windowHeight || googleposY < 0){
+    googleposYspeed= googleposYspeed * -1;
   }
-  background (100,40,100);
+  if (googleposX > windowWidth || googleposX < 0){
+    googleposXspeed= googleposXspeed * -1;
+  }
+  googleposY = googleposY - googleposYspeed;
+  googleposX = googleposX - googleposXspeed;
+  //googleposX = googleposX + random(-.8,10);
+  //googleposY = googleposY + random(-.8,.8);
 
-  if(mouseX > windowWidth/2 && mouseY < windowHeight/2){
 
-      ellipseX = 300;
-      ellipseY = 200;
-  } else {
-      ellipseX = 900;
-      ellipseY = 500;
+
+
+
+  //INSTAGRAMMMMM
+  if (igposY > windowHeight || igposY < 0){
+    igposYspeed= igposYspeed * -1;
   }
 
+igposY = igposY + igposYspeed;
+  igposX = igposX + random(-.8,.8);
+  //igposY = igposY + random(-.8,.8);
 
-  fill(30,20,50);
-  ellipse (ellipseX, ellipseY, 100,100);
+
+
 }
-
-
 
 
 //when window is resized fixing!!!
-function windowResized(){
+function windowResized(){ ///////////////////////////////////////////////////
   resizedCanvas(windowWidth, windowHeight);
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,19 +1,44 @@
+
+let ellipseX  = 0;
+let ellipseY = 0;
+
+let pointX = 400;
+let pointY = 200;
 function setup() {
   //setup code goes once
   createCanvas(windowWidth,windowHeight);
-  background(180,0,30);
+  background(random(255),random(255),random(255));
+  rectMode(CENTER);
+
+  print(random(300));
+
+  ellipseX= random(ellipseX, windowWidth);
+  ellipseY= random(ellipseY, windowHeight);
+
+
 }
 
 function draw() {
   // draw code goes in a loop
 
 
+ellipse(ellipseX,ellipseY,150,150);
 fill(30,255,60);
 strokeWeight(3);
 ellipse(400,500,300);
 noFill();
 rect(500,400,200,200);
 
-rect(mouseX, mouseY, 10,10);
+//rect(mouseX, mouseY, 10,10);
+fill(random(255));
+stroke(30);
+strokeWeight(30);
+line(30,30, 30,300);
+stroke(random(255),random(255),random(255));
+
+point(mouseX, mouseY);
+point(pointX, pointY);
+strokeWeight(20);
+
 
 }
