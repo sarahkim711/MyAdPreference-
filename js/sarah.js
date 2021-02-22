f398002544d74802fe6036f5c19d88c3871f0ffe
sarahkim711 <ykim134@artic.edu>
2021-02-21 21:14:52 -0600

    main menu DUN, fb vid DUN

--- js/sarah.js
+++ js/sarah.js
@@ -1,6 +1,10 @@
 //main menu circle
-let circleButton1X;
-let circleButton1Y;
+let fbButtonX;
+let fbButtonY;
+let googleButtonX;
+let googleButtonY;
+let igButtonX;
+let igButtonY;
 
 //return to main menu ellipse
 let mainMenuReturnX;
@@ -9,64 +13,126 @@ let mainMenuReturnY;
 let canvas;
 
 //video variable
-let transitionVid;
+let fbTransitionVid;
+let googleTransitionVid;
+let igTransitionVid;
 
 //booleans to draw functions
 let mainMenuBool = true;
 let adFacebookBool = false;
+let adGoogleBool = false;
+let adIgBool = false;
 
 let fb;
 let fbposYspeed = 3;
+let fbposXspeed = 3;
+
+let google;
+let googleposYspeed = 4;
+let googleposXspeed = 3;
+
+let ig;
+let igposYspeed = 3;
+
+// let mycursor;
+// let mycursorX = mouseX;
+// let mycursorY = mouseY;
+
 let backgroundimgX =0
 let backgroundimgY =0
+let splash;
+let blob;
+let rainbow;
+let mycursor;
+
+
+let bottomleft;
+let bottomleftX;
+let bottomleftY;
+
+let bottomright;
+let topleft;
+let topright;
 
 
 function preload(){
+  //mycursor = loadImage(("images/middlefin.png");
   fb = loadImage("images/fb1swirl.png");
+  google = loadImage("images/g2swirl.png");
   backgroundimg = loadImage ("images/backgroundswirl.png");
-}
-
-function facebook (){
-
-  circleButton1X = windowWidth/6;
-  circleButton1Y = windowHeight/2;
-
-    image(fb, circleButton1X, circleButton1Y,250,250);
-
-    if (circleButton1Y > windowHeight || circleButton1YY < 0){
-      fbposYspeed= fbposYspeed * -1;
-    }
-  circleButton1Y = circleButton1Y + fbposYspeed;
-  circleButton1X = circleButton1Y + random(-10,10);
-
-
-
+  ig = loadImage("images/ig3swirl.png");
+  bottomleft = loadImage("images/bottomleft.png");
+  bottomright = loadImage ("images/bottomright.png");
+  topleft = loadImage("images/topleft.png");
+  topright = loadImage("images/topright.png");
+  splash = loadImage("images/splash.png");
+  blob = loadImage("images/blob.png");
+  rainbow = loadImage("images/rainbow.png");
+  mycursor = loadImage("images/middlefin.png");
 
 }
+
+
 function setup() {
 
   canvas = createCanvas(windowWidth, windowHeight);
+  //below makes main menu background black
+  background(0);
+//   imageMode(CENTER);
+  // // image (splash,windowWidth/2, windowHeight/3, windowWidth,windowHeight);
+  //  image (blob,windowWidth/2, windowHeight/2, windowWidth,windowHeight);
+  //  imageMode(CORNER);
+  // image (rainbow,-150,-150, windowWidth*1.2,windowHeight*1.2);
   canvas.position(0,0);
   canvas.style("z-index", "-1");
 
 
   //create your video, style it then hide it until we want to show it
   //you need to add your own video here.
-  transitionVid = createVideo(['videos/Playblast 1:2.mov']);
-  transitionVid.style('z-index', '2');
-  transitionVid.style('position', 'relative');
-  transitionVid.style('width', '100%');
-  transitionVid.style('height', '100%');
-  transitionVid.hide();
+  //make one for each F,G,I
+  fbTransitionVid = createVideo(['videos/facebookSPIN.mov']);
+  fbTransitionVid.style('z-index', '2');
+  // fbTransitionVid.style('position', 'relative');
+  fbTransitionVid.position (windowWidth/10,windowHeight/10);
+  fbTransitionVid.style('width', '80%');
+  fbTransitionVid.style('height', '70%');
+  fbTransitionVid.hide();
+
+  googleTransitionVid = createVideo(['videos/dad.mov']);
+  googleTransitionVid.style('z-index', '2');
+  googleTransitionVid.style('position', 'relative');
+  googleTransitionVid.style('width', '100%');
+  googleTransitionVid.style('height', '100%');
+  googleTransitionVid.hide();
+
+  igTransitionVid = createVideo(['videos/dog.mov']);
+  igTransitionVid.style('z-index', '2');
+  igTransitionVid.style('position', 'relative');
+  igTransitionVid.style('width', '100%');
+  igTransitionVid.style('height', '100%');
+  igTransitionVid.hide();
 
   //setting the main menu and return to main menu buttons x and y coordinates
-  circleButton1X = windowWidth/6;
-  circleButton1Y = windowHeight/2;
+  fbButtonX = windowWidth/6;
+  fbButtonY = windowHeight/2;
 
-  backgroundimgX = windowWidth/2
-  backgroundimgY = windowHeight/2
-  imageMode(CENTER);
-  image (backgroundimg,backgroundimgX, backgroundimgY, windowWidth,windowHeight);
+  googleButtonX = windowWidth;
+  googleButtonY = 20;
+//  googleButtonX = random(windowWidth);
+//  googleButtonY = random(windowHeight);
+
+  // igButtonX= windowWidth/1.5;
+  igButtonX = random(windowWidth);
+  igButtonY = windowHeight/2;
+  // imageMode(CENTER);
+  // for(let i=0; i <50; i++){
+  //   backgroundimgX = random(windowWidth);
+  //   backgroundimgY = random(windowHeight);
+  //   image(backgroundimg, backgroundimgX, backgroundimgY, 100,100);
+  // }
+   backgroundimgX = windowWidth/2;
+   backgroundimgY = windowHeight/2;
+
 
   mainMenuReturnX = 60;
   mainMenuReturnY = 70;
@@ -75,51 +141,134 @@ function setup() {
 //the main menu function
 function menuButtons(){
   //make sure proper booleans are flipped
-
+  //background animating
+  // image (backgroundimg,backgroundimgX, backgroundimgY, random(windowWidth),random(windowHeight));
+//   image (splash,windowWidth/2, windowHeight/3, windowWidth,windowHeight);
   mainMenuBool = true;
   adFacebookBool = false;
+  adGoogleBool = false;
+  adIgBool = false;
 
 //  background(255);
-imageMode(CENTER);
-//  //ellipse(circleButton1X, circleButton1Y, 100, 100);
-image(fb,circleButton1X, circleButton1Y, 200, 200);
+  imageMode(CENTER);
+  imageMode(CORNER);
+  // image (splash,windowWidth/2, windowHeight/3, windowWidth,windowHeight);
+  image (blob,windowWidth/9, windowHeight/7, windowWidth/2,windowHeight/2);
+  imageMode(CORNER);
+  image (rainbow,-150,-150, windowWidth*1.2,windowHeight*1.2);
+  imageMode(CENTER);
+  image(bottomright,windowWidth*8/9,windowHeight*7/9, 500, 500);
+  image(topleft, windowWidth*2/9,windowHeight*3/9,650,500);
+  image(topright, windowWidth*7/9, windowHeight*2/9,windowWidth/2,windowHeight/2);
+  image(bottomleft, windowWidth*1/9, windowHeight*7/9,500,650)
 
-/////////////////////
-if(dist(mouseX, mouseY, mainMenuReturnX, mainMenuReturnY)< 25 && mouseIsPressed){
-  menuButtons();
-}
-if (circleButton1Y > windowHeight || circleButton1Y < 0){
+  imageMode(CENTER);
+  //  image(mycursorX,mycursorY,20,20);
+  frameRate(50);
+  //  //ellipse(circleButton1X, circleButton1Y, 100, 100);
+  image(fb,fbButtonX, fbButtonY, 500,500);
+  image(google, googleButtonX, 300,300);
+  image(ig, igButtonX, igButtonY, 500,500);
+  /////////////////////the animations down below
+  //FACEBOOK ANIMATION
+
+  // if (fbButtonY > windowHeight || fbButtonY < 0){
+  //   fbposYspeed= fbposYspeed * -1;
+  // }
+  // fbButtonY = fbButtonY + fbposYspeed;
+  // fbButtonX = fbButtonX + random(-10,10);
+  if (fbButtonY > windowHeight || fbButtonY < 0){
   fbposYspeed= fbposYspeed * -1;
-}
+  }
+  if (fbButtonX > windowWidth || fbButtonX < 0){
+    fbposXspeed= fbposXspeed * -1;
+  }
+  fbButtonY = fbButtonY - fbposYspeed;
+  fbButtonX = fbButtonX - fbposXspeed;
 
-circleButton1Y = circleButton1Y + fbposYspeed;
-circleButton1X = circleButton1X + random(-10,10);
-
+  //GOOGLE ANIMATION
+  if (googleButtonY > windowHeight || googleButtonY < 0){
+    googleposYspeed= googleposYspeed * -1;
+  }
+  if (googleButtonX > windowWidth || googleButtonX < 0){
+    googleposXspeed= googleposXspeed * -1;
+  }
+  googleButtonY = googleButtonY - googleposYspeed;
+  googleButtonX = googleButtonX - googleposXspeed;
+  //INSTAGRAM animations
+  if (igButtonY > windowHeight || igButtonY < 0){
+    igposYspeed= igposYspeed * -1;
+  }
+  igButtonY = igButtonY + igposYspeed;
+  igButtonX = igButtonX + random(-.8,.8);
   //check to see if the mouse is over the menu ellipse, if it is and mouse
   //is pressed trigger the videoTransition function
-  if(dist(mouseX, mouseY, circleButton1X, circleButton1Y)< 100 && mouseIsPressed){
-    videoTransition1();
+  //make one for each F,G,I
+  if(dist(mouseX, mouseY, fbButtonX, fbButtonY)< 250 && mouseIsPressed){
+    fbvideoTransition();
+  }
+  if(dist(mouseX, mouseY, googleButtonX, googleButtonY)< 250 && mouseIsPressed){
+    googlevideoTransition();
+  }
+  if(dist(mouseX, mouseY, igButtonX,igButtonY)< 250 && mouseIsPressed){
+    igvideoTransition();
   }
 }
 
-
-function videoTransition1(){
+//make one for each F,G,I
+function fbvideoTransition(){
+  //backgroundcolor
+  background(200,20,10);
   //make sure proper booleans are flipped
   mainMenuBool = false;
   adFacebookBool = false;
 
   //show the video and play it
-  transitionVid.show();
-  transitionVid.style('display', 'inline');
-  transitionVid.play();
+  fbTransitionVid.show();
+  fbTransitionVid.style('display', 'inline');
+  fbTransitionVid.play();
 
   //when the video has ended, trigger the asPref1 function
-  transitionVid.onended(adFacebook);
+  fbTransitionVid.onended(adFacebook);
 }
+
+function googlevideoTransition(){
+
+  background(0);
+  //make sure proper booleans are flipped
+  mainMenuBool = false;
+  adFacebookBool = false;
+
+  //show the video and play it
+  googleTransitionVid.show();
+  googleTransitionVid.style('display', 'inline');
+  googleTransitionVid.play();
+
+  //when the video has ended, trigger the asPref1 function
+  googleTransitionVid.onended(adGoogle);
+}
+function igvideoTransition(){
+
+  background(0);
+  //make sure proper booleans are flipped
+  mainMenuBool = false;
+  adIgBool = false;
+
+  //show the video and play it
+  igTransitionVid.show();
+  igTransitionVid.style('display', 'inline');
+  igTransitionVid.play();
+
+  //when the video has ended, trigger the asPref1 function
+  igTransitionVid.onended(adInstagram);
+}
+
+
+
 
 function adFacebook(){
   //hide the video
-  transitionVid.hide();
+  fbTransitionVid.hide();
 
   //make sure proper booleans are flipped
   mainMenuBool = false;
@@ -127,13 +276,58 @@ function adFacebook(){
 
 
   background(255);
-  text("Animated Screen", 50, 200);
+  text("Facebook", 50, 200);
   ellipse(mainMenuReturnX, mainMenuReturnY, 50, 50);
   ellipse(mouseX, mouseY, 30,30);
 
   //check to see if the mouse is over the return menu ellipse, if it is and mouse
   //is pressed return to the main menu
+  if(dist(mouseX, mouseY, mainMenuReturnX, mainMenuReturnY)< 25 && mouseIsPressed){
+    menuButtons();
+  }
+}
 
+function adGoogle(){
+  //hide the video
+  googleTransitionVid.hide();
+
+  //make sure proper booleans are flipped
+  mainMenuBool = false;
+  adGoogleBool = true;
+
+
+  background(255);
+  text("Google", 50, 200);
+  ellipse(mainMenuReturnX, mainMenuReturnY, 50, 50);
+  ellipse(mouseX, mouseY, 30,30);
+
+  //check to see if the mouse is over the return menu ellipse, if it is and mouse
+  //is pressed return to the main menu
+  if(dist(mouseX, mouseY, mainMenuReturnX, mainMenuReturnY)< 25 && mouseIsPressed){
+    menuButtons();
+  }
+}
+
+
+function adInstagram(){
+  //hide the video
+  igTransitionVid.hide();
+
+  //make sure proper booleans are flipped
+  mainMenuBool = false;
+  adIgBool = true;
+
+
+  background(255);
+  text("Instagram", 50, 200);
+  ellipse(mainMenuReturnX, mainMenuReturnY, 50, 50);
+  ellipse(mouseX, mouseY, 30,30);
+
+  //check to see if the mouse is over the return menu ellipse, if it is and mouse
+  //is pressed return to the main menu
+  if(dist(mouseX, mouseY, mainMenuReturnX, mainMenuReturnY)< 25 && mouseIsPressed){
+    menuButtons();
+  }
 }
 
 function draw() {
@@ -144,6 +338,7 @@ function draw() {
 
   //if the main menu boolean is true, keep drawing the menuButtons function
 
+
   if(mainMenuBool == true){
     menuButtons();
   }
@@ -153,6 +348,14 @@ function draw() {
   if(adFacebookBool == true){
     adFacebook();
   }
+  if(adGoogleBool == true){
+    adGoogle();
+  }
+  if(adIgBool == true){
+    adInstagram();
+  }
+  imageMode(CENTER);
+    image(mycursor,mouseX, mouseY, 100,100);
 
 
 }
--- js/sketch.js
+++ js/sketch.js
@@ -22,23 +22,23 @@ function draw() {
   // draw code goes in a loop
 
 
-ellipse(ellipseX,ellipseY,150,150);
-fill(30,255,60);
-strokeWeight(3);
-ellipse(400,500,300);
-noFill();
-rect(500,400,200,200);
+// ellipse(ellipseX,ellipseY,150,150);
+// fill(30,255,60);
+// strokeWeight(3);
+// ellipse(400,500,300);
+// noFill();
+// rect(500,400,200,200);
 
-//rect(mouseX, mouseY, 10,10);
+rect(mouseX, mouseY, 10,10);
 fill(random(255));
 stroke(30);
 strokeWeight(30);
 line(30,30, 30,300);
-stroke(random(255),random(255),random(255));
+stroke(100,200,300);
 
-point(mouseX, mouseY);
-point(pointX, pointY);
-strokeWeight(20);
+// point(mouseX, mouseY);
+// point(pointX, pointY);
+// strokeWeight(20);
 
 
 }
